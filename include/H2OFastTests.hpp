3dfc0c6f65918edd178826ca21f0753a97e6aff9
Michaël Roynard <michaelroynard@gmail.com>
2017-02-04 02:29:33 +0100

    Remove non standard workaround

--- include/H2OFastTests.hpp
+++ include/H2OFastTests.hpp
@@ -33,15 +33,6 @@
 namespace H2OFastTests {
     // Implementation details
     namespace detail {
-        //XCode workaround
-#if __APPLE__
-        template<typename T, typename ... Args>
-        std::unique_ptr<T> make_unique(Args&& ...args) {
-            return std::unique_ptr<T>{ new T{ std::forward<Args>(args)... } };
-        }
-#else
-        using std::make_unique;
-#endif
 
         template<class Type>
         struct type_helper {
